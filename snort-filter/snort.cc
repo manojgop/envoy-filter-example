c2c4e56d4f7fceb24217e284d8643532085066f9
Manoj Gopalakrishnan <manoj.gopalakrishnan@intel.com>
2025-02-25 07:07:32 -0700

    Analyze data and inject data to filter chain after processing
    Signed-off-by: Manoj Gopalakrishnan <manoj.gopalakrishnan@intel.com>

--- snort-filter/snort.cc
+++ snort-filter/snort.cc
@@ -9,6 +9,7 @@
 namespace Envoy {
 namespace Filter {
 
+// Snort Filter Config
 SnortFilterConfig::SnortFilterConfig(
     const snort::SnortConfig& proto_config, Stats::Scope& scope)
     : stat_prefix_(proto_config.stat_prefix()),
@@ -34,20 +35,16 @@ std::unique_ptr<CidrRange> SnortFilterConfig::getRemoteIP(const snort::SnortConf
   return nullptr;
 }
 
-Snort::Snort(SnortFilterConfigSharedPtr config)
-    : config_(config) {}
-
-
-Network::FilterStatus Snort::onData(Buffer::Instance& data, bool) {
-  ENVOY_CONN_LOG(trace, "snort: got {} bytes", read_callbacks_->connection(), data.length());
+// Analyze
+void Analyzer::analyze(Buffer::Instance& data, bool end_stream) {
   config_->stats().total_.inc();
   const std::string &connection_remote_ip
-    = read_callbacks_->connection().connectionInfoProvider().remoteAddress()->ip()->addressAsString();
+    = connection_.connectionInfoProvider().remoteAddress()->ip()->addressAsString();
   bool match = true;
   if(config_->remoteIP() != nullptr) {
     const std::string &config_remote_ip = config_->remoteIP()->ip()->addressAsString();
     ENVOY_CONN_LOG(trace, "snort: config remote IP : {}, connection remote IP : {}",
-      read_callbacks_->connection(), config_remote_ip, connection_remote_ip);
+      connection_, config_remote_ip, connection_remote_ip);
     if(config_remote_ip != connection_remote_ip) {
        match = false;
     }
@@ -56,17 +53,85 @@ Network::FilterStatus Snort::onData(Buffer::Instance& data, bool) {
   if((match && config_->action() == snort::SnortConfig_Action_DENY) ||
       (!match && config_->action() == snort::SnortConfig_Action_ALLOW)) {
     ENVOY_CONN_LOG(trace, "snort: Denied connection from remote IP : {}",
-        read_callbacks_->connection(), connection_remote_ip);
+      connection_, connection_remote_ip);
     config_->stats().denied_.inc();
-    read_callbacks_->connection().close(Network::ConnectionCloseType::NoFlush, "snort_deny_close");
-    return Network::FilterStatus::StopIteration;
+    connection_.close(Network::ConnectionCloseType::NoFlush, "snort_deny_close");
+    if (timer_->enabled()) {
+      timer_->disableTimer();
+    }
+    return;
   }
-
   ENVOY_CONN_LOG(trace, "snort: Allowed connection from remote IP : {}",
-      read_callbacks_->connection(), connection_remote_ip);
+    connection_, connection_remote_ip);
 
   config_->stats().allowed_.inc();
-  return  Network::FilterStatus::Continue;
+
+  buffer_.move(data);
+  end_stream_ |= end_stream;
+  if (!timer_->enabled()) {
+    timer_->enableTimer(tick_interval_);
+  }
+}
+
+void Analyzer::reset() {
+  ENVOY_CONN_LOG(trace, "snort: Reset",connection_);
+  timer_->disableTimer();
+}
+
+void Analyzer::onTimerTick() {
+  Buffer::OwnedImpl next_chunk{};
+  if (0 < buffer_.length()) {
+    auto chunk_length = max_chunk_length_ < buffer_.length() ? max_chunk_length_ : buffer_.length();
+    next_chunk.move(buffer_, chunk_length);
+  }
+  bool end_stream = end_stream_ && 0 == buffer_.length();
+  if (0 < buffer_.length()) {
+    timer_->enableTimer(tick_interval_);
+  }
+  next_chunk_cb_(next_chunk, end_stream);
+}
+
+
+// Snort Filter
+Snort::Snort(SnortFilterConfigSharedPtr config)
+    : config_(config) {
+  tick_interval_ = std::chrono::milliseconds(1);
+  max_chunk_length_= 256;
+}
+
+void Snort::initializeReadFilterCallbacks(Network::ReadFilterCallbacks& callbacks) {
+  read_callbacks_ = &callbacks;
+  read_callbacks_->connection().addConnectionCallbacks(*this);
+
+  read_analyzer_ = std::make_unique<Analyzer>(
+      config_, read_callbacks_->connection(), tick_interval_, max_chunk_length_,
+      [this](Buffer::Instance& data, bool end_stream) {
+        read_callbacks_->injectReadDataToFilterChain(data, end_stream);
+      });
+}
+
+Network::FilterStatus Snort::onData(Buffer::Instance& data, bool end_stream) {
+  ENVOY_CONN_LOG(trace, "snort: got {} bytes", read_callbacks_->connection(), data.length());
+  ENVOY_CONN_LOG(trace,
+    "snort: connectionInfoProvider remote address : {},\
+    downstreamAddressProvider remote address : {},\
+    downstreamAddressProvider direct remote address : {}",
+    read_callbacks_->connection(),
+    read_callbacks_->connection().connectionInfoProvider().remoteAddress()->asString(),
+    read_callbacks_->connection().streamInfo().downstreamAddressProvider().remoteAddress()->asString(),
+    read_callbacks_->connection().streamInfo().downstreamAddressProvider().directRemoteAddress()->asString()
+  );
+
+  read_analyzer_->analyze(data, end_stream);
+
+  return Network::FilterStatus::StopIteration;
+}
+
+void Snort::onEvent(Network::ConnectionEvent event) {
+  if (event == Network::ConnectionEvent::RemoteClose ||
+      event == Network::ConnectionEvent::LocalClose) {
+    read_analyzer_->reset();
+  }
 }
 
 } // namespace Filter
--- snort-filter/snort.h
+++ snort-filter/snort.h
@@ -1,11 +1,14 @@
 #pragma once
 
-
+#include "envoy/event/dispatcher.h"
+#include "envoy/event/timer.h"
+#include "envoy/network/connection.h"
 #include "envoy/network/filter.h"
 #include "snort-filter/snort.pb.h"
 #include "envoy/stats/stats_macros.h"
 #include "source/common/common/logger.h"
 #include "source/common/network/cidr_range.h"
+#include "source/common/buffer/buffer_impl.h"
 
 namespace Envoy {
 namespace Filter {
@@ -27,6 +30,8 @@ struct SnortStats {
 };
 
 using CidrRange = Envoy::Network::Address::CidrRange;
+//using Connection = Envoy::Network::Connection;
+using ConnectionSharedPtr = std::shared_ptr<Envoy::Network::Connection>;
 
 class SnortFilterConfig {
 public:
@@ -49,22 +54,67 @@ private:
 using SnortFilterConfigSharedPtr = std::shared_ptr<SnortFilterConfig>;
 
 /**
+ * Traffic analayzer using snort.
+ * emits a next chunk of the original request/response data on timer tick.
+ */
+class Analyzer : public Logger::Loggable<Logger::Id::filter> {
+public:
+Analyzer(SnortFilterConfigSharedPtr config, Network::Connection &connection,
+            std::chrono::milliseconds tick_interval, uint64_t max_chunk_length,
+            std::function<void(Buffer::Instance&, bool)> next_chunk_cb)
+      : config_(config), connection_(connection),
+        timer_(connection.dispatcher().createTimer([this] { onTimerTick(); })),
+        tick_interval_(tick_interval),
+        max_chunk_length_(max_chunk_length), next_chunk_cb_(next_chunk_cb) {}
+
+  /**
+   * Analyze given given request/response data.
+   */
+  void analyze(Buffer::Instance& data, bool end_stream);
+  /**
+   * Cancel any scheduled activities (on connection close).
+   */
+  void reset();
+
+private:
+  void onTimerTick();
+
+  Buffer::OwnedImpl buffer_{};
+  bool end_stream_{};
+
+  const SnortFilterConfigSharedPtr config_;
+  Network::Connection &connection_;
+  const Event::TimerPtr timer_;
+  const std::chrono::milliseconds tick_interval_;
+  const uint64_t max_chunk_length_;
+  const std::function<void(Buffer::Instance&, bool)> next_chunk_cb_;
+};
+
+
+/**
  * Implementation of a basic snort filter.
  */
-class Snort : public Network::ReadFilter, Logger::Loggable<Logger::Id::filter> {
+class Snort : public Network::ReadFilter, Network::ConnectionCallbacks,
+              Logger::Loggable<Logger::Id::filter> {
 public:
   Snort(SnortFilterConfigSharedPtr);
 
   // Network::ReadFilter
   Network::FilterStatus onData(Buffer::Instance& data, bool end_stream) override;
   Network::FilterStatus onNewConnection() override { return Network::FilterStatus::Continue; }
-  void initializeReadFilterCallbacks(Network::ReadFilterCallbacks& callbacks) override {
-    read_callbacks_ = &callbacks;
-  }
+  void initializeReadFilterCallbacks(Network::ReadFilterCallbacks& callbacks) override;
+
+  // Network::ConnectionCallbacks
+  void onEvent(Network::ConnectionEvent event) override;
+  void onAboveWriteBufferHighWatermark() override {}
+  void onBelowWriteBufferLowWatermark() override {}
 
 private:
   const SnortFilterConfigSharedPtr config_;
+  std::chrono::milliseconds tick_interval_;
+  uint64_t max_chunk_length_;
   Network::ReadFilterCallbacks* read_callbacks_{};
+  std::unique_ptr<Analyzer> read_analyzer_;
 };
 
 } // namespace Filter
